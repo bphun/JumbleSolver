e5fdf13f4020e05072e87433ff3680c12f0e7923
Brandon <Brandon.phan.73@gmail.com>
2017-04-17 15:41:07 -0700

    Added android application

---
+++ JumbleSolver_Android/app/src/main/java/com/brandonphan/jumblesolver_android/JumbleSolverActivity.java
@@ -0,0 +1,13 @@
+package com.brandonphan.jumblesolver_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JumbleSolverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jumble_solver);
+    }
+}
